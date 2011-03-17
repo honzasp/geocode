a238bf366d5daae9234bfea2a2bf52f60b740458
Honza Špaček <patek.mail@gmail.com>
2011-03-17 14:36:54 +0100

    Add support for names.

--- geocode.rb
+++ geocode.rb
@@ -12,15 +12,21 @@ input = File.open INPUT_NAME
 output = File.open OUTPUT_NAME, "w"
 
 #addresses = [ "Bohuslava Martinů Ostrava-Poruba", "1600 Amphitheatre Parkway, Mountain View, CA" ]
-addresses = input.readlines.map &:strip
 
 output.puts "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 output.puts "<loc version=\"1.0\">"
 output.puts
 
-addresses.each do |address|
-	next if address.empty?
-	$stdout.print "#{address}... "
+input.readlines.map(&:strip).each do |line|
+	next if line.empty?
+
+	if line =~ /^([^:]*):(.*)$/
+		name, address = $1.strip, $2.strip
+	else
+		address = line.strip
+	end
+
+	$stdout.print "#{name+": " if name}#{address} ... "
 	$stdout.flush
 
 	uri = URI.parse "http://maps.googleapis.com/maps/api/geocode/json?address=#{URI.encode(address)}&sensor=false"
@@ -74,7 +80,7 @@ addresses.each do |address|
 	
 	if coords
 		output.puts "<waypoint>"
-		output.puts "  <name><![CDATA[#{address}]]></name>"
+		output.puts "  <name><![CDATA[#{name or address}]]></name>"
 		output.puts "  <coord lat=\"#{coords["lat"]}\" lon=\"#{coords["lng"]}\"/>"
 		output.puts "</waypoint>"
 		output.puts
